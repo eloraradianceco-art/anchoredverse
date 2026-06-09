d90f2b964c78e5a29f7dd0ef085272a92efcf0e1
eloraradianceco-art <eloraradiance.co@gmail.com>
2026-06-08 22:50:19 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AnchoredVerse from "./AnchoredVerse.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AnchoredVerse />
+  </React.StrictMode>
+);
